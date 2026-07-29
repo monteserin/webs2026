6a8db004188b960c66c6cb8943c902a279bb1332
Pablo <paramonty@gmail.com>
2026-07-29 14:02:25 +0100

    subida

---
+++ 04-blucles/05-tabla-multiplicar-01.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 10; i++) {
+  const r = 7 * i;
+  console.log("7 * " + i + " = " + r);
+}
---
+++ 04-blucles/05-tabla-multiplicar-02.js
@@ -0,0 +1,6 @@
+for (let j = 1; j <= 10; j++) {
+  for (let i = 1; i <= 10; i++) {
+    const r = j * i;
+    console.log(j + " * " + i + " = " + r);
+  }
+}

725f878a76077974cdff0bd2c405622f1b8f4760
Pablo <paramonty@gmail.com>
2026-09-10 08:39:53 +0100

    subida

---
+++ repaso/console.log.js
@@ -0,0 +1,13 @@
+let variable = 99;
+console.log(variable);
+
+//////////////////////////
+
+let nombre = "Paco";
+console.log(nombre);
+///////////////
+
+let a;
+a = 7;
+let b = a;
+console.log(b); // Debería mostrarse el valor de a

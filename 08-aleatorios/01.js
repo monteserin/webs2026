31665d8df945e1541f85436ae78b1b4b324e4f0f
Pablo <paramonty@gmail.com>
2026-09-18 09:57:27 +0100

    subida

---
+++ 08-aleatorios/01.js
@@ -0,0 +1,3 @@
+const v = Math.floor(Math.random() * (7 - 5 + 1)) + 5;
+
+console.log(v);
---
+++ 08-aleatorios/02.js
@@ -0,0 +1,5 @@
+const nombre = "Juan";
+const n = Math.floor(Math.random() * nombre.length);
+console.log(n);
+console.log(nombre[n]);
+console.log(nombre.charAt(n));
